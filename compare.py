da5ab2b155251bead32c5c07f486eac0714daf9b
Eva Morrison <eva.morrison33@ku.edu>
2021-07-22 17:41:52 -0500

    compare based on the repmask ID only

---
+++ compare.py
@@ -0,0 +1,68 @@
+from pprint import pprint
+import re
+
+strain9_file = "newLocation9.bed"
+strain160_file = "newLocation160.bed"
+newFile = None
+
+def parse_strain160(fname):
+    with open(fname, "r") as fh:
+
+        for line in fh:
+            line = line.strip()
+            values = line.split()
+            chromosome = values[0]
+            seq_start = values[1]
+            seq_end = values[2]
+            seq_ID= values[3]
+
+            #return the chromosome, sequence start position and sequence end position and the sequence ID
+            #the sequence ID contains the repeatmasker ID, nanopore ID, and TE element name
+            yield chromosome, seq_start, seq_end, seq_ID, values
+            #yield values
+
+def find_match(fname,seq_name):
+    with open(fname, "r") as fh:
+        chromosome = ""
+        seq_start = '\0'
+        seq_end = '\0'
+        seq_ID= ""
+
+        for line in fh:
+            line = line.strip()  # Remove trailing newline characters.
+            values = line.split()
+            chromosome = values[0]
+            seq_start = values[1]
+            seq_end = values[2]
+            seq_ID= values[3]
+            if(seq_ID==seq_name):
+                #print (values)
+                yield values, seq_ID
+        return(values, seq_ID)
+
+
+if newFile:
+        newFile.close()
+new_filename = 'newInsertions.bed'
+newFile = open(new_filename, "w")
+
+for entry in parse_strain160(strain160_file):
+    chromosome = entry[0]
+    left_coord = entry[1]
+    right_coord = entry[2]
+    seq_ID = entry[3]
+    found160 = entry[4]
+    
+    for match in find_match(strain9_file, seq_ID):
+        
+        found9 = match[0]
+        str160 = ("\t".join(found160))
+        newFile.write(str160+"\n")
+        str9 = ("\t".join(found9))
+        newFile.write(str9+"\n")
+        
+    
+    #print ("Chromosome: " + chromosome + "\tSequence ID: "+seq_ID+"\tLeft: "+left_coord+"\tRight: "+right_coord+"\n")
+
+
+        
