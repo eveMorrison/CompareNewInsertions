e7e27b304c9dcb6675dfb19b0b5dbb7c9556df94
Eva Morrison <eva.morrison33@ku.edu>
2021-07-23 16:58:57 -0500

    make changes to match requirements

--- compare.py
+++ compare.py
@@ -1,9 +1,11 @@
+from pickle import FALSE, TRUE
 from pprint import pprint
 import re
 
 strain9_file = "newLocation9.bed"
 strain160_file = "newLocation160.bed"
 newFile = None
+newInsert_file = None
 
 def parse_strain160(fname):
     with open(fname, "r") as fh:
@@ -38,16 +40,32 @@ def find_match(fname,seq_name, seq_len160):
 
             seq_len9 = int(seq_end) - int(seq_start)
 
+            #if(seq_ID == seq_name):
+            #    yield values, seq_ID
             if(seq_ID == seq_name and seq_len160 == seq_len9):
-                #print (values)
-                yield values, seq_ID
+               yield values, seq_ID
         return(values, seq_ID)
+
+def findRepeats(fname,arr):
+    newInsert_file = fname
+    print("160:" + arr[1])
+    with open(fname, "r") as fh:
+        for line in fh:
+            line = line.strip()
+            values = line.split()            
+            if(values[0] == arr[0] and values[1] == arr[1]):
+                return 1
+    return 0
+    print(newInsert_file)
+
 
 
 if newFile:
         newFile.close()
-new_filename = 'newInsertionsLeng.bed'
+new_filename = 'newInsertionsStrict.bed'
 newFile = open(new_filename, "w")
+
+previousRead = ""
 
 for entry in parse_strain160(strain160_file):
     chromosome = entry[0]
@@ -59,12 +77,22 @@ for entry in parse_strain160(strain160_file):
     seqLen160 = int(right_coord) - int(left_coord)
     
     for match in find_match(strain9_file, seq_ID, seqLen160):
-        
-        found9 = match[0]
-        str160 = ("\t".join(found160))
-        newFile.write(str160+"\n")
-        str9 = ("\t".join(found9))
-        newFile.write(str9+"\n")
+        if(previousRead != found160[1]):
+            found9 = match[0]
+            str160 = ("\t".join(found160))
+            newFile.write(str160+"\n")
+            str9 = ("\t".join(found9))
+            newFile.write(str9+"\n")
+            previousRead = found160[1]
+        else:
+            print(previousRead)
+        #exists = findRepeats(new_filename,found160)
+        #if(exists == 0 ):
+        #    found9 = match[0]
+        #    str160 = ("\t".join(found160))
+        #    newFile.write(str160+"\n")
+        #    str9 = ("\t".join(found9))
+        #    newFile.write(str9+"\n")
         
     
     #print ("Chromosome: " + chromosome + "\tSequence ID: "+seq_ID+"\tLeft: "+left_coord+"\tRight: "+right_coord+"\n")
